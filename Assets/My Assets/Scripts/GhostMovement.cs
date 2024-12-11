73abb4ecf71bf6caafe5dbd0e7619513d98c7288
Cloud <michalcloud2003@gmail.com>
2024-12-11 20:57:58 +0000

    :sparkles: Added movement to the ghost in a circular pattern

--- Assets/My Assets/Scripts/GhostMovement.cs
+++ Assets/My Assets/Scripts/GhostMovement.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class GhostMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Transform target; //Target object to rotate around
+    public float speed = 2f; //speed of the movement
+    public float radius = 1f; //radius of the path
+    public float angle = 0f; //current angle of the object
 
     // Update is called once per frame
     void Update()
     {
-        
+        float x = target.position.x + Mathf.Cos(angle) * radius;
+        float y = target.position.y;
+        float z = target.position.z + Mathf.Sin(angle) * radius;
+
+        transform.position = new Vector3(x, y, z);
+
+        angle += speed * Time.deltaTime;
     }
 }
